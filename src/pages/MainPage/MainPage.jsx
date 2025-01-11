803da2dd242a1fadfa337422efc5b400c72d391d
Minji Kim <minjikim2000@hotmail.com>
2025-01-12 05:19:51 +0900

    Design: 메인페이지 UI 구현 (#31)

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,31 @@
+import StatusBar from '@/components/StatusBar/StatusBar';
 import * as S from '@/pages/MainPage/MainPage.style';
+import ConstellationCard from '@/components/ConstellationCard/ConstellationCard';
+import MainTodo from '@/components/MainTodo/MainTodo';
+
+const constellationsData = [
+  { userId: 1, username: '유저명', image: 'https://i.ibb.co/c3Xx12S/star-user.png', rank: 4 },
+  { userId: 2, username: '유저명2', image: 'https://i.ibb.co/0hBsLVD/star-yellow1.png', rank: 1 },
+  { userId: 3, username: '유저명3', image: 'https://i.ibb.co/JnTTQwp/star-yellow2.png" alt="star-yellow2', rank: 2 },
+  { userId: 4, username: '유저명4', image: 'https://i.ibb.co/Qm8rvgw/star-grey1.png', rank: 5 },
+  { userId: 5, username: '유저명5', image: 'https://i.ibb.co/0hBsLVD/star-yellow1.png', rank: 3 },
+  { userId: 6, username: '유저명6', image: 'https://i.ibb.co/cvB7MxK/star-grey2.png', rank: 6 },
+];
 
 const MainPage = () => {
-  return <S.Container>메인 페이지</S.Container>;
+  return (
+    <>
+      <StatusBar rank={4} participants={6} />
+      <S.Container>
+        {constellationsData.map(({ userId, username, image, rank }) => (
+          <ConstellationCard key={userId} userId={userId} username={username} image={image} rank={rank} />
+        ))}
+      </S.Container>
+      <S.TodoSection>
+        <MainTodo />
+      </S.TodoSection>
+    </>
+  );
 };
 
 export default MainPage;
--- src/pages/MainPage/MainPage.style.js
+++ src/pages/MainPage/MainPage.style.js
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 12px;
+  padding: 0 20px;
+  padding-top: 8px;
+  justify-items: center;
+`;
+
+export const TodoSection = styled.div`
+  display: flex;
+  justify-content: center;
+`;
