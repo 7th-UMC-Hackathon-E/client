0281ce561b6b057ba0d478612ad327afddf0bc43
Minji Kim <minjikim2000@hotmail.com>
2025-01-11 23:03:41 +0900

    Design: 디자인 시스템 구축 (#16)

---
+++ src/components/Typography.js
@@ -0,0 +1,31 @@
+import styled, { css } from 'styled-components';
+
+const typographyStyles = {
+  h1: css`
+    font-size: ${({ theme }) => theme.font.size.h1};
+    font-weight: ${({ theme }) => theme.font.weight.semibold};
+  `,
+  h2: css`
+    font-size: ${({ theme }) => theme.font.size.h2};
+    font-weight: ${({ theme }) => theme.font.weight.bold};
+  `,
+  bodyDefault: css`
+    font-size: ${({ theme }) => theme.font.size.bodyDefault};
+    font-weight: ${({ theme }) => theme.font.weight.regular};
+  `,
+  bodyTitle: css`
+    font-size: ${({ theme }) => theme.font.size.bodyTitle};
+    font-weight: ${({ theme }) => theme.font.weight.medium};
+  `,
+  cardTitle: css`
+    font-size: ${({ theme }) => theme.font.size.cardTitle};
+    font-weight: ${({ theme }) => theme.font.weight.medium};
+  `,
+};
+
+const Typography = styled.p`
+  font-family: ${({ theme }) => theme.font.family};
+  ${({ variant }) => typographyStyles[variant] || typographyStyles.h2}
+`;
+
+export default Typography;
