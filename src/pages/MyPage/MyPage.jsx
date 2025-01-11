c71f27127174212f18f91a526682d8b140be2cda
Eunjin Oh <ibsuny3214@naver.com>
2025-01-12 00:43:23 +0900

    Design: MyPage 디자인 완성(#21)

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,7 +1,23 @@
 import * as S from '@/pages/MyPage/MyPage.style';
 
 const MyPage = () => {
-  return <S.Container>메인 페이지</S.Container>;
+  return (
+    <S.Container>
+      <S.TimerContainer>
+        <S.UnionImage src="/images/mypage_union.png" alt="Union Image" />
+        <S.Timer>00:00:00</S.Timer>
+        <S.UnionImage src="/images/mypage_union.png" alt="Union Image" />
+      </S.TimerContainer>
+      <S.mainUnionImage src="/images/mypage_mainUnion.png" alt="Main Union Image" />
+      <S.nameDiv>
+        <S.name>이름</S.name>
+      </S.nameDiv>
+      <S.ButtonsContainer>
+        <S.Button>ToDo 작성</S.Button>
+        <S.Button>이전 기록 보기</S.Button>
+      </S.ButtonsContainer>
+    </S.Container>
+  );
 };
 
 export default MyPage;
