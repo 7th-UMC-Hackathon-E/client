1d7f515dace241d94e7bdb65902d143e5b061710
dohyung <154952683+dohyung001@users.noreply.github.com>
2025-01-11 22:04:24 +0900

    Feat: 로그인페이지(스플래쉬) UI 구현

--- src/pages/SplashPage/SplashPage.jsx
+++ src/pages/SplashPage/SplashPage.jsx
@@ -1,7 +1,40 @@
 import * as S from '@/pages/SplashPage/SplashPage.style';
+import { useState } from 'react';
 
 const SplashPage = () => {
-  return <S.Container>메인 페이지</S.Container>;
+  const [input, setInput] = useState({
+    name: '',
+    birth: '',
+  });
+
+  // 입력
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  return (
+    <S.Container>
+      <S.Title>
+        <p>서비스명</p>
+      </S.Title>
+      <S.Image src="/images/login_img.png" alt="Login Image" />
+      <S.Form>
+        <S.FormGroup>
+          <p>이름</p>
+          <input name="name" value={input.name} onChange={handleInput} placeholder="이름을 입력하세요" />
+        </S.FormGroup>
+        <S.FormGroup>
+          <p>생년월일</p>
+          <input name="birth" value={input.birth} onChange={handleInput} type="date" placeholder="YYYY-MM-DD" />
+        </S.FormGroup>
+        <S.Button>서비스 입장하기</S.Button>
+      </S.Form>
+    </S.Container>
+  );
 };
 
 export default SplashPage;
