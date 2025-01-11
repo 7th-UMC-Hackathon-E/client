27b5e4c2e49b5dd2b20eda197875ed969fe2c4b0
Eunjin Oh <ibsuny3214@naver.com>
2025-01-12 03:42:35 +0900

    Feat: apis 파일 생성 (#34)

---
+++ src/apis/index.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+
+const baseUrl = import.meta.env.VITE_BASEURL;
+
+export const api = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  // baseURL: import.meta.env.DEV ? 'http://localhost:5173/' : import.meta.env.VITE_BASEURL,
+  baseURL: baseUrl,
+});
+
+export const getUserDetail = async () => {
+  const response = await api.get('/api/user/detail');
+  return response;
+};
+
+// export const postSignUp = async (requset) => {
+//   const response = await api.post('/auth/signUp', requset);
+
+//   return response;
+// };
+
+// export const postEmailDuplication = async (request) => {
+//   const response = await api.post('/auth/emailDuplication', request);
+
+//   return response;
+// };
+
+// export const postEmailVerification = async (request) => {
+//   const response = await api.post('/auth/emailVerification', request);
+
+//   return response;
+// };
+
+// export const postLogin = async (request) => {
+//   const response = await api.post('/auth/login', request);
+
+//   return response;
+// };
+
+// export const postMail = async (id, request) => {
+//   const response = await api.post(`/mail/${id}`, request);
+//   return response;
+// };
+
+// export const getFriendMail = async (id, { page, size }) => {
+//   const response = await api.get(`/friendMail/${id}?page=${page}&size=${size}`);
+//   return response;
+// };
+
+// export const getMyMail = async (id, filter, { page, size }) => {
+//   const response = await api.get(`/myMail/${id}?filter=${filters[filter]}?page=${page}&size=${size}`);
+//   return response;
+// };
+
+// export const getSearch = async (q) => {
+//   const response = await api.get(`/search?keyword=${q}`);
+//   return response.data;
+// };
+
+// export const getKWTreeMail = async () => {
+//   const response = await api.get('/kwTree/mailBoxList');
+//   console.log(response);
+//   return response.data;
+// };
