b4eb313e3fb7895003b06cdfca8c6af9047e79c2
Minji Kim <minjikim2000@hotmail.com>
2025-01-12 07:47:58 +0900

    Feat: Card 컴포넌트에 네비게이션 연결 (#39)

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,24 +1,32 @@
+import { useNavigate } from 'react-router-dom';
 import * as S from '@/components/Card/Card.style';
 import LockIcon from '@/assets/icons/lock.svg';
 
 const Card = ({ isActive, title, participants, maxParticipants, bgImage, bottomTitle, starIcon }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate('/main');
+  };
   return (
-    <S.CardWrapper>
-      <S.CardContainer isActive={isActive} bgImage={bgImage}>
-        {!isActive ? (
-          <S.LockIconStyled src={LockIcon} alt="Lock Icon" />
-        ) : (
-          <>
-            <S.Participants>
-              {participants}명 / {maxParticipants}명
-            </S.Participants>
-            <S.StarIconStyled src={starIcon} alt="Star Icon" />
-          </>
-        )}
-        <S.TitleButton isActive={isActive}>{title}</S.TitleButton>
-      </S.CardContainer>
-      <S.BottomTitle>{bottomTitle}</S.BottomTitle>
-    </S.CardWrapper>
+    <button onClick={handleNavigate} style={{ all: 'unset' }}>
+      <S.CardWrapper>
+        <S.CardContainer isActive={isActive} bgImage={bgImage}>
+          {!isActive ? (
+            <S.LockIconStyled src={LockIcon} alt="Lock Icon" />
+          ) : (
+            <>
+              <S.Participants>
+                {participants}명 / {maxParticipants}명
+              </S.Participants>
+              <S.StarIconStyled src={starIcon} alt="Star Icon" />
+            </>
+          )}
+          <S.TitleButton isActive={isActive}>{title}</S.TitleButton>
+        </S.CardContainer>
+        <S.BottomTitle>{bottomTitle}</S.BottomTitle>
+      </S.CardWrapper>
+    </button>
   );
 };
 
