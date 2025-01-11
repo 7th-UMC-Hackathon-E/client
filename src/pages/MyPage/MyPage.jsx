81e27a67c4f627ece614a389473687e9cfd57b02
dohyung <154952683+dohyung001@users.noreply.github.com>
2025-01-11 20:05:57 +0900

    Revert "Chore: 파일 구조 변경"

--- src/pages/MyPage/MyPage.jsx
+++
@@ -1,7 +0,0 @@
-import * as S from '@/pages/Mypage/MyPage.style';
-
-const MyPage = () => {
-  return <S.Container>메인 페이지</S.Container>;
-};
-
-export default MyPage;
---
+++ src/pages/RecordPage.jsx
@@ -0,0 +1,5 @@
+const RecordPage = () => {
+  return <h1>기록 페이지</h1>;
+};
+
+export default RecordPage;
