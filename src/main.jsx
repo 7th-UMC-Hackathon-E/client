cdfd5ce6e1f23fc92e04ca9be987a25d767d69e3
Eunjin Oh <ibsuny3214@naver.com>
2025-01-11 22:19:49 +0900

    Chore: MSW 세팅, test 완료 (#15)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+// src/index.js
+import { worker } from '@/mocks/browser';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// if (import.meta.env.DEV) {
+//   const { worker } = await import('./mocks/browser.js');
+//   worker.start();
+// }
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser.js');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+);
