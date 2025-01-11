a2257e9464e1a18313b8127513f98df79da26d24
dohyung <154952683+dohyung001@users.noreply.github.com>
2025-01-12 05:50:45 +0900

    Fix: 이전기록 페이지 수정

--- src/pages/RecordPage/RecordPage.style.js
+++ src/pages/RecordPage/RecordPage.style.js
@@ -4,8 +4,8 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   padding: 26px;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-image: url('/images/backgroundBlue.png');
   background-size: cover;
   background-position: center;
