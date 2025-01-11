1804eeaeffd50776349cee06b97fb6f77ea50591
dohyung <154952683+dohyung001@users.noreply.github.com>
2025-01-12 07:38:37 +0900

    Feat: 메인 페이지 체크 시 이미지 전환

--- src/components/MainTodo/MainTodo.jsx
+++ src/components/MainTodo/MainTodo.jsx
@@ -10,7 +10,7 @@ const todosData = [
   { id: 3, title: '할 일 3', isCompleted: false },
 ];
 
-const MainTodo = () => {
+const MainTodo = ({ updateImages }) => {
   const [todos, setTodos] = useState(todosData);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
@@ -18,6 +18,7 @@ const MainTodo = () => {
   const handleNavigate = () => {
     navigate('/mypage');
   };
+
   const toggleTodo = (id) => {
     const updatedTodos = todos.map((todo) => (todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo));
     setTodos(updatedTodos);
@@ -29,7 +30,10 @@ const MainTodo = () => {
   };
 
   useEffect(() => {
-    if (calculateProgress() === 100) {
+    const progress = calculateProgress();
+    updateImages(progress);
+
+    if (progress === 100) {
       setIsModalOpen(true);
     }
   }, [todos]);
@@ -54,22 +58,18 @@ const MainTodo = () => {
           <S.ModalContent>
             <S.ModalHeader>오늘의 목표 달성 레포트</S.ModalHeader>
             <S.PercentageText>100% 달성</S.PercentageText>
-
             <S.TodoStats>
               완료한 todo : {todos.length}개<br />
               완료하지 못한 todo : 0개
             </S.TodoStats>
-
             <S.CongratsMessage>
               완벽한 하루였어요!
               <br />
               목표를 100% 달성했네요. 대단해요.
               <br />이 흐름을 이어가면 더 큰 성취를 만들 수 있을 거예요! 🎉
             </S.CongratsMessage>
-
             <S.FeedbackLabel>오늘 당신의 노력은 빛났어요! 하고 싶은 말을 마음껏 남겨보세요!</S.FeedbackLabel>
             <S.FeedbackInput placeholder="응원의 메시지를 입력하세요." />
-
             <S.CloseButton onClick={handleNavigate}>종료하기</S.CloseButton>
           </S.ModalContent>
         </S.ModalOverlay>
