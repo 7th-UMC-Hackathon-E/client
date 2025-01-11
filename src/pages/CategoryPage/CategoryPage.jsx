07243f8bfdde58670767465f63336bfe09d3d35c
Minji Kim <minjikim2000@hotmail.com>
2025-01-12 06:35:58 +0900

    Design: 버튼 UI 수정 (#39)

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import Typography from '@/components/Typography';
 import * as S from '@/pages/CategoryPage/CategoryPage.style';
 import CardList from '@/components/CardList/CardList';
+import { Button } from '@/components/common/Button/Button';
 
 const CategoryPage = () => {
   const navigate = useNavigate();
@@ -11,11 +12,11 @@ const CategoryPage = () => {
   };
   return (
     <S.Container>
-      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
         <Typography varient="h2" style={{ paddingLeft: 33, marginBottom: 20 }}>
           Group
         </Typography>
-        <button onClick={handleNavigate}>오늘의 To Do</button>
+        <Button text="오늘의 To Do" onClick={handleNavigate} />
       </div>
       <CardList />
     </S.Container>
